171222958d612bcde2bfb1c9cbf92aeafd8e7f8a
CooperR1 <cooperreed7@gmail.com>
2023-09-14 10:01:27 -0700

    More Vercel Compatibility

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from myapp.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    # path("", include("myapp.urls"))
+    path("", include("myapp.urls"))
 ]
