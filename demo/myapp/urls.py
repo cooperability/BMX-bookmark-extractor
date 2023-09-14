6cbd68357d3012773f8f61ecba7f2909779dd849
CooperR1 <cooperreed7@gmail.com>
2023-09-14 09:50:43 -0700

    Vercel Compatibility

--- demo/myapp/urls.py
+++ demo/myapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("todos/", views.todos, name="Todos")
+    # path("", views.home, name="home"),
+    path("todos/", views.todos, name="Todos"),
+    path("index/", views.index, name="Index")
 ]
--- demo/myapp/views.py
+++ demo/myapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from .models import TodoItem
+from datetime import datetime
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -7,6 +9,20 @@ from .models import TodoItem
 def home(request):
     return render(request, "home.html")
 
+
 def todos(request):
     items = TodoItem.objects.all()
     return render(request, "todos.html", {"todos": items})
+
+
+def index(request):
+    now = datetime.now()
+    html = f'''
+    <html>
+        <body>
+            <h1>This should work</h1>
+            <p>The current time is { now }.</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
