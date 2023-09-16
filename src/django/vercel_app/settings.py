09bbee1dc623299bb29618bb2c171c4e073abd5a
CooperR1 <cooperreed7@gmail.com>
2023-09-15 22:32:27 -0700

    Django updates 1

--- src/django/vercel_app/settings.py
+++ src/django/vercel_app/settings.py
@@ -78,8 +78,18 @@ WSGI_APPLICATION = 'vercel_app.wsgi.app'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        # 'ENGINE': 'django.db.backends.postgresql',
+
+        # 'NAME': '<db_name>',
+
+        # 'USER': '<db_username>',
+
+        # 'PASSWORD': '<password>',
+
+        # 'HOST': '<db_hostname_or_ip>',
+
+        # 'PORT': '<db_port>',
     }
 }
 
