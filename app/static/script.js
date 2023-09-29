f626b3825b9bac5ee46b4f51d7aec836b14383d1
CooperR1 <cooperreed7@gmail.com>
2023-09-28 23:57:53 -0700

    Better error checking, displacy improvements, stopping the regular GET call
    Better error checking, displacy improvements, stopping the regular GET call

--- app/static/script.js
+++ app/static/script.js
@@ -1,34 +1,64 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const urlForm = document.getElementById("urlForm");
-  const summaryElement = document.getElementById("summary");
-  const entitiesElement = document.getElementById("entities");
-  const sentimentElement = document.getElementById("sentiment");
-  const rawContentElement = document.getElementById("rawContent");
+//DOM Element References
+const urlForm = document.getElementById("urlForm");
+const urlInput = document.getElementById("urlInput");
+const summaryElement = document.getElementById("summary");
+const lengthInput = document.getElementById("length");
+const entitiesElement = document.getElementById("entities");
+const sentimentElement = document.getElementById("sentiment");
+const rawContentElement = document.getElementById("rawContent");
+const moreEntitiesButton = document.getElementById("moreEntities");
 
-  urlForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-
-    // Display a loading message to the user
-    summaryElement.textContent = "Fetching and processing data...";
-
-    const urlInput = document.getElementById("urlInput").value;
-    const lengthInput = document.getElementById("length").value;
-
+async function fetchDataAndUpdateUI(url, length) {
+  try {
     const response = await fetch(
-      `/process?url=${encodeURIComponent(urlInput)}&length=${lengthInput}`
+      `/process?url=${encodeURIComponent(url)}&length=${length}`
     );
     const data = await response.json();
 
     summaryElement.textContent = data.summary;
     entitiesElement.innerHTML = data.entities
-      .map((entity) => `<li>${entity}</li>`)
+      .map(
+        (entity) =>
+          `<li>${entity.text} (${entity.count} times) - ${entity.label_desc}</li>`
+      )
       .join("");
-    sentimentElement.textContent = data.sentiment;
+    const displacyDiv =
+      document.getElementById("displacy") || document.createElement("div");
+    displacyDiv.id = "displacy";
+    displacyDiv.innerHTML = data.displacy;
+    entitiesElement.after(displacyDiv);
+    sentimentElement.textContent = "Sentiment: " + data.sentiment;
     rawContentElement.textContent = data.rawContent;
-  });
+  } catch (error) {
+    summaryElement.textContent = "Error fetching data. Please try again.";
+  }
+}
 
-  // Update displayed summary length when the slider is moved
-  document.getElementById("length").addEventListener("input", (e) => {
-    document.getElementById("lengthValue").textContent = e.target.value;
-  });
+urlForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  summaryElement.textContent = "Fetching and processing data...";
+  fetchDataAndUpdateUI(urlInput.value, lengthInput.value);
+});
+
+// Update displayed summary length when the slider is moved
+lengthInput.addEventListener("change", (e) => {
+  document.getElementById("lengthValue").textContent = e.target.value;
+  fetchDataAndUpdateUI(urlInput.value, e.target.value);
+});
+
+moreEntitiesButton.addEventListener("click", async () => {
+  const urlInput = document.getElementById("urlInput").value;
+
+  // Fetch top 50 entities this time
+  const response = await fetch(
+    `/process?url=${encodeURIComponent(urlInput)}&length=500`
+  );
+  const data = await response.json();
+
+  entitiesElement.innerHTML = data.entities
+    .map(
+      (entity) =>
+        `<li>${entity.text} (${entity.count} times) - ${entity.label_desc}</li>`
+    )
+    .join("");
 });
